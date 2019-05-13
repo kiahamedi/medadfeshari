4d7ebc1280f107c881d42424bce978dfb93c4875
Shahab KarimiFar <shahabkarimi7033@yahoo.com>
2019-05-13 16:39:20 +0430

    Design Backstage

--- front end/bootstrap/js/index.js
+++ front end/bootstrap/js/index.js
@@ -1,6 +1,6 @@
-    $(document).ready(function() {
+$(document).ready(function() {
      
-      $("#owl-example").owlCarousel({
+    $("#owl-example").owlCarousel({
       	'items' : 1,
       	'itemsDesktop':1,
       	'itemsDesktopSmall':1,
@@ -9,6 +9,10 @@
       	'navigation':true,
       	'pagination':true,
       	'navigationText':["داستان قبلی","داستان بعدی"]
-      });
-	      
-    });
+    });        
+});
+
+$('.step .one .arrow i').click(function(){
+    $(this).toggleClass('actives');
+    $(this).parents('.step').find('.author').slideToggle(150);
+});
