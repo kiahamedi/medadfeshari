31bbd21134da84d85495fad31e12474f9bda9df3
Shahab KarimiFar <shahabkarimi7033@yahoo.com>
2019-05-16 00:20:39 +0430

    add donate page

--- front end/bootstrap/js/index.js
+++ front end/bootstrap/js/index.js
@@ -1,18 +1,42 @@
 $(document).ready(function() {
      
     $("#owl-example").owlCarousel({
-      	'items' : 1,
-      	'itemsDesktop':1,
-      	'itemsDesktopSmall':1,
-      	'itemsTablet':1,
-      	'itemsMobile':1,
-      	'navigation':true,
-      	'pagination':true,
-      	'navigationText':["داستان قبلی","داستان بعدی"]
+       'items' : 1,
+       'itemsDesktop':1,
+       'itemsDesktopSmall':1,
+       'itemsTablet':1,
+       'itemsMobile':1,
+       'navigation':true,
+       'pagination':true,
+       'navigationText':["ﺩاﺳﺘﺎﻥ ﻗﺒﻞی","ﺩاﺳﺘﺎﻥ ﺑﻌﺪی"]
     });        
+
 });
 
-$('.step .one .arrow i').click(function(){
-    $(this).toggleClass('actives');
-    $(this).parents('.step').find('.author').slideToggle(150);
-});
+// $('.step .one .arrow i').click(function(){
+//     $(this).toggleClass('actives');
+//     $(this).parents('.step').find('.author').slideToggle(150);
+// });
+
+
+// $(function(){
+
+//    $('#submitinput').click(function(){
+//       var nameContent = $('#nameinput').val();
+//       var emailContent = $('#emailinput').val();
+//       var priceContent = $('#priceinput').val();
+//            if(nameContent == ''){
+//               $('.messagebox').fadeIn('slow').delay(2000).fadeOut('.messagebox');                
+//             }else if(emailContent == ''){
+//               $('.messagebox').fadeIn('slow').delay(2000).fadeOut('.messagebox');
+//             }else if(priceContent == ''){
+//               $('.messagebox').fadeIn('slow').delay(2000).fadeOut('.messagebox');
+//             }            
+//    });
+
+// });
+
+$('.btn-support').click(function(){
+      $('.support-table').slideToggle(500);
+    });
+
