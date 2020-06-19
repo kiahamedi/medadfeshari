ae932dd89967b5645f40f3cd6e37dd9791e5e37e
Kia Hamedi <kia.arta9793@gmail.com>
2020-06-19 15:43:36 +0430

    change style login/register HTML

--- main/forms.py
+++ main/forms.py
@@ -16,6 +16,7 @@ class NewUserForm(UserCreationForm):
 	class Meta:
 		model = User
 		fields = ("username","email","password1","password2")
+		
 
 	def save(self,commit=True):
 		user = super(NewUserForm,self).save(commit=False)
